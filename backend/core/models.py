9fa388e4504faa41fb3c1a9ae775b19adfefbe8a
riya9818 <reeyabasnet999@gmail.com>
2025-12-05 22:16:37 +0545

    models customer entity made

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Customer(models.Model):
+    account_manager = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='managed_customers'
+    )
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    joined_date = models.DateField()
+    is_active = models.BooleanField(default=True)
+    country = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
